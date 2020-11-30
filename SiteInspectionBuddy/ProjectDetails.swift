11d509e64e4ba405e1244c6b416e9da0d6eaf4be
Spencer Feng <spencer.feng@icloud.com>
2020-11-30 20:22:30 +1100

    Add 'Edit Project Info' list item in ProjectDetails screen and create a placeholder ProjectInfo screen

--- SiteInspectionBuddy/ProjectDetails.swift
+++ SiteInspectionBuddy/ProjectDetails.swift
@@ -10,9 +10,41 @@ import SwiftUI
 struct ProjectDetails: View {
     let project: Project
     
+    @State var selectedListItem: String? = nil
+    
     @Environment(\.managedObjectContext) var managedObjectContext
     
+    init(project: Project) {
+        self.project = project
+        UITableViewCell.appearance().selectionStyle = .none
+    }
+    
     var body: some View {
-        Text("Project details")
+        let EDIT_PROJECT_INFO = "Edit Project Info"
+        
+        List {
+            Section {
+                NavigationLink(
+                    destination: ProjectInfo(),
+                    tag: EDIT_PROJECT_INFO,
+                    selection: $selectedListItem
+                ) {
+                    Button(action: {
+                        self.selectedListItem = EDIT_PROJECT_INFO
+                    }) {
+                        HStack {
+                            Image(systemName: "info.circle").foregroundColor(.black)
+                            Text(EDIT_PROJECT_INFO).foregroundColor(.black)
+                        }
+                    }
+                }
+                .listRowBackground(self.selectedListItem == EDIT_PROJECT_INFO ? Color(red: 242/255, green: 242/255, blue: 242/255) : Color.clear)
+            }
+        }
+        .onDisappear {
+            self.selectedListItem = nil
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationBarTitle(Text("Details"), displayMode: .inline)
     }
 }
---
+++ SiteInspectionBuddy/ProjectInfo.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectInfo.swift
+//  SiteInspectionBuddy
+//
+//  Created by Spencer Feng on 30/11/20.
+//
+
+import SwiftUI
+
+struct ProjectInfo: View {
+    var body: some View {
+        Text("Project info")
+    }
+}
+
+struct ProjectInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectInfo()
+    }
+}
