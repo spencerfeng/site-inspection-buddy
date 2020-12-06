d595b29ce082239b18120dd54884adde29a67c4b
Spencer Feng <spencer.feng@icloud.com>
2020-12-06 14:26:06 +1100

    Create a struct to store global constants

---
+++ SiteInspectionBuddy/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SiteInspectionBuddy
+//
+//  Created by Spencer Feng on 6/12/20.
+//
+
+import SwiftUI
+
+struct Constants {
+    static let DEFAULT_MARGIN: CGFloat = 16
+    static let DEFAULT_BUTTON_HEIGHT: CGFloat = 44
+}
