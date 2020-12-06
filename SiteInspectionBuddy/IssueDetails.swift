aa045cb3392b58fd347a5599112841b391c65c8f
Spencer Feng <spencer.feng@icloud.com>
2020-12-06 14:29:27 +1100

    Add the buttons to add photos for an issue in Issue Details screen

--- SiteInspectionBuddy/IssueDetails.swift
+++ SiteInspectionBuddy/IssueDetails.swift
@@ -10,8 +10,43 @@ import SwiftUI
 struct IssueDetails: View {
     var issue: Issue
     
+    var hasPhotos: Bool {
+        return issue.photosArray.count > 0
+    }
+    
+    @ViewBuilder
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationBarTitle(Text(issue.title!), displayMode: .inline)
+        GeometryReader { geometry in
+            HStack {
+                Button(action: {
+                    // TODO: show camera
+                }) {
+                    HStack {
+                        Image(systemName: "camera").foregroundColor(.black)
+                        Text("Camera").foregroundColor(.black)
+                    }
+                }
+                .frame(width: (geometry.size.width - 3 * Constants.DEFAULT_MARGIN) / 2.0, height: Constants.DEFAULT_BUTTON_HEIGHT)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1)
+                )
+                Spacer()
+                Button(action: {
+                    // TODO: show photo gallery
+                }) {
+                    HStack {
+                        Image(systemName: "photo.on.rectangle").foregroundColor(.black)
+                        Text("Gallery").foregroundColor(.black)
+                    }
+                }
+                .frame(width: (geometry.size.width - 3 * Constants.DEFAULT_MARGIN) / 2.0, height: Constants.DEFAULT_BUTTON_HEIGHT)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1)
+                )
+            }
+            .padding(.horizontal, Constants.DEFAULT_MARGIN)
+            .padding(.vertical, 20)
+        }
+        .navigationBarTitle(Text(issue.title!), displayMode: .inline)
     }
 }
