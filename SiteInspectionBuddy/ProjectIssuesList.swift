0dbd352ac8f6ebb2b0973626abbf1a932feb1e59
Spencer Feng <spencer.feng@icloud.com>
2021-01-09 10:11:51 +1100

    Allow the user to delete issues

--- SiteInspectionBuddy/ProjectIssuesList.swift
+++ SiteInspectionBuddy/ProjectIssuesList.swift
@@ -53,22 +53,31 @@ struct ProjectIssuesList: View {
                     ProjectIssuesListItem(issue: issue, selectedIssueId: $selectedIssueId)
                     .listRowBackground(self.selectedIssueId == issue.id ? Color(red: 242/255, green: 242/255, blue: 242/255) : Color.clear)
                 }
+                .onDelete(perform: deleteIssues)
+                .onMove(perform: moveIssue)
             }
             .onDisappear {
                 self.selectedIssueId = nil
             }
         }
         .navigationBarTitle(Text("Issues"), displayMode: .inline)
-        .navigationBarItems(trailing: Button(action: {
-            withAnimation(.easeInOut(duration: 1.0)) {
-                let newIssue = addIssue()
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    self.selectedIssueId = newIssue.id
-                }
-            }
-        }) {
-            Image(systemName: "plus")
-        })
+        .navigationBarItems(trailing: EditButton())
+    }
+    
+    func deleteIssues(at offsets: IndexSet) {
+        for index in offsets {
+            let issue = issues[index]
+            managedObjectContext.delete(issue)
+        }
+
+        // TODO: find the reason why we can not call saveContext directly
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            saveContext()
+        }
+    }
+    
+    func moveIssue(from source: IndexSet, to destination: Int) {
+        // TODO: reorder issues
     }
     
     func addIssue() -> Issue {
